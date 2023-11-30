f90b633f39693435e8ee95c027622cc5cd63d90c
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-11-30 21:28:59 +0100

    improve slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Carousel } from 'primeng/carousel';
 import { MovieService } from '../_services/movie.service';
 import { Movie } from '../_models/movie.model';
-import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-your-component',
@@ -9,13 +10,27 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   movies: Movie[] = [];
-  
+  visibleMovies: Movie[] = [];
+  currentIndex = 0;
+
+  @ViewChild('carousel') carousel!: Carousel;
+
   constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
     this.movieService.getMovies().subscribe((movies) => {
       this.movies = movies;
+      this.updateVisibleMovies();
     });
   }
 
+  updateVisibleMovies(): void {
+    this.visibleMovies = this.movies.slice(this.currentIndex, this.currentIndex + 5);
+  }
+
+  scrollCarousel(direction: 'left' | 'right'): void {
+    const increment = direction === 'left' ? -5 : 5;
+    this.currentIndex = (this.currentIndex + increment + this.movies.length) % this.movies.length;
+    this.updateVisibleMovies();
+  }
 }
